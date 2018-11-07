43dcb1817661f1abe2a6205bdcaaea7dcb43e568
Daniel Lipovetsky <daniel@platform9.com>
2018-11-06 17:27:27 -0800

    Move kubeadm to v1alpha2

--- defaults/defaults.go
+++ defaults/defaults.go
@@ -6,7 +6,8 @@ import (
 	"github.com/platform9/nodeadm/apis"
 	"github.com/platform9/nodeadm/constants"
 	"github.com/platform9/nodeadm/workarounds"
-	kubeadmv1alpha1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	kubeadmv1alpha2 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha2"
 )
 
 // SetInitDefaults sets defaults on the configuration used by init
@@ -16,11 +17,11 @@ func SetInitDefaults(config *apis.InitConfiguration) {
 	// Second set MasterConfiguration.Networking defaults
 	SetMasterConfigurationNetworkingDefaultsWithNetworking(config)
 	// Third use the remainder of MasterConfiguration defaults
-	kubeadmv1alpha1.SetDefaults_MasterConfiguration(&config.MasterConfiguration)
+	kubeadmv1alpha2.SetDefaults_MasterConfiguration(&config.MasterConfiguration)
 	config.MasterConfiguration.Kind = "MasterConfiguration"
-	config.MasterConfiguration.APIVersion = "kubeadm.k8s.io/v1alpha1"
+	config.MasterConfiguration.APIVersion = "kubeadm.k8s.io/v1alpha2"
 	config.MasterConfiguration.KubernetesVersion = constants.KubernetesVersion
-	config.MasterConfiguration.NoTaintMaster = true
+	config.MasterConfiguration.NodeRegistration.Taints = []corev1.Taint{} // empty slice denotes no taints
 	addOrAppend(&config.MasterConfiguration.APIServerExtraArgs, "feature-gates", constants.FeatureGates)
 	addOrAppend(&config.MasterConfiguration.ControllerManagerExtraArgs, "feature-gates", constants.FeatureGates)
 	addOrAppend(&config.MasterConfiguration.SchedulerExtraArgs, "feature-gates", constants.FeatureGates)
@@ -32,7 +33,7 @@ func SetInitDynamicDefaults(config *apis.InitConfiguration) error {
 	if err != nil {
 		return fmt.Errorf("unable to dervice hostname override: %v", err)
 	}
-	config.MasterConfiguration.NodeName = nodeName
+	config.MasterConfiguration.NodeRegistration.Name = nodeName
 	return nil
 }
 
